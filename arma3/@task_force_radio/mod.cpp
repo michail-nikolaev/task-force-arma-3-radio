a3641ab7a872c48be553343b7730274b7c73048f
Michail Nikolaev <michail.nikolaev@gmail.com>
2013-07-27 09:49:08 +0300

    filter fixes, logo, fixes 55

--- arma3/@task_force_radio/mod.cpp
+++ arma3/@task_force_radio/mod.cpp
@@ -1,1 +1,5 @@
 name = "Task Force Arrowhead Radio";
+picture = "task_force_arrowhead_logo.paa";
+actionName = "Website";
+action = "https://github.com/michail-nikolaev/task-force-arma-3-radio#task-force-arma-3-radio";
+description = "Task Force Arrowhead TeamSpeak Radio Plugin";
