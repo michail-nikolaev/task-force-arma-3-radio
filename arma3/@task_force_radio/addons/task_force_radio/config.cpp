42e0418f6a28f3b68396810accb303cdc00e6b5e
Michail Nikolaev <michail.nikolaev@gmail.com>
2013-08-06 01:12:36 +0300

    support for user config, fixes #20

--- arma3/@task_force_radio/addons/task_force_radio/config.cpp
+++ arma3/@task_force_radio/addons/task_force_radio/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches
 		weapons[] = {};
 		requiredVersion = 1.0;
 		requiredAddons[] = {"Extended_EventHandlers"};
-		author[] = {"Nkey"};
+		author[] = {"[TF]Nkey"};
 		authorUrl = "https://github.com/michail-nikolaev/task-force-arma-3-radio";
 	};
 };
@@ -16,5 +16,5 @@ class Extended_PostInit_EventHandlers
 	task_force_radio_Post_Init = "if !(isDedicated) then { call compile preProcessFileLineNumbers '\task_force_radio\init.sqf' }";
 };
 
-
+#include "\userconfig\task_force_radio\radio_keys.hpp"
 #include "\task_force_radio\description.h"
---
+++ arma3/userconfig/task_force_radio/radio_keys.hpp
@@ -0,0 +1,244 @@
+﻿class task_force_radio_keys {
+
+	class tanget_sw {
+		key = 58;
+		ctrl = 0;
+		shift = 0;
+		alt = 0;
+	};
+
+	class dialog_sw {
+		key = 22;
+		ctrl = 0;
+		shift = 0;
+		alt = 0;
+	};
+
+};
+
+/* KEY LIST // Tastendruckzuweisung // Le key de la baguette // Обозначения клавиш // Zorro key // Lista klawiszy // Billentyû lista // whatever */
+/* ================================================================================================================= */
+/*
+		KEY LIST FOR US KEYBOARD LAYOUT BELOW THIS LIST
+
+
+shift
+ctrl
+alt
+
+ESC = 1 
+F1 = 59 
+F2 = 60 
+F3 = 61 
+F4 = 62 
+F5 = 63 
+F6 = 64 
+F7 = 65 
+F8 = 66 
+F9 = 67 
+F10 = 68 
+F11 = 87 
+F12 = 88 
+PRINT = 183 
+SCROLL = 70 
+PAUSE = 197 
+^ = 41 
+1 = 2 
+2 = 3 
+3 = 4 
+4 = 5 
+5 = 6 
+6 = 7 
+7 = 8 
+8 = 9 
+9 = 10 
+0 = 11 
+ß = 12 
+´ = 13 
+Ü = 26 
+Ö = 39 
+Ä = 40 
+# = 43 
+< = 86 
+, = 51 
+. = 52 
+- = 53 
++ = NOT DEFINED
+POS1 = 199 
+TAB = 15 
+ENTER = 28 
+DELETE = 211 
+BACKSPACE = 14 
+INSERT = 210 
+END = 207 
+PAGEUP = 201 
+PAGEDOWN = 209 
+CAPS = 58 
+A = 30 
+B = 48 
+C = 46 
+D = 32 
+E = 18 
+F = 33 
+G = 34 
+H = 35 
+I = 23 
+J = 36 
+K = 37 
+L = 38 
+M = 50 
+N = 49 
+O = 24 
+P = 25 
+Q = 16 
+U = 22 
+R = 19 
+S = 31 
+T = 20 
+V = 47 
+W = 17 
+X = 45 
+Y = 44 
+Z = 21 
+SHIFTL = 42 
+SHIFTR = 54 
+UP = 200 
+DOWN = 208 
+LEFT = 203 
+RIGHT = 205 
+NUM_0 = 82 
+NUM_1 = 79 
+NUM_2 = 80 
+NUM_3 = 81 
+NUM_4 = 75 
+NUM_5 = 76 
+NUM_6 = 77 
+NUM_7 = 71 
+NUM_8 = 72 
+NUM_9 = 73 
+NUM_+ = 78 
+NUM = 69 
+NUM_/ = 181 
+NUM_* = 55 
+NUM_- = 74 
+NUM_, = 83 
+NUM_ENTER = 156 
+STRGL = 29 
+STRGR = 157  
+WINL = 220 
+WINR = 219  
+ALT = 56 
+SPACE = 57 
+ALTGR = 184 
+APP = 221 
+*/
+
+
+/* KEY LIST // English (US) 104-key    provided by tcp */ 
+/* ================================================================================================================= */
+/*
+ESC = 1 
+F1 = 59 
+F2 = 60 
+F3 = 61 
+F4 = 62 
+F5 = 63 
+F6 = 64 
+F7 = 65 
+F8 = 66 
+F9 = 67 
+F10 = 68 
+F11 = 87 
+F12 = 88 
+PRINT = 183 
+SCROLL = 70 
+PAUSE = 197 
+` = 41  
+1 = 2 
+2 = 3 
+3 = 4 
+4 = 5 
+5 = 6 
+6 = 7 
+7 = 8 
+8 = 9 
+9 = 10 
+0 = 11 
+- = 12 
+= = 13 
+, = 51 
+. = 52 
+/ = 53
+; = 39
+' = 40
+[ = 26
+] = NOT DEFINED
+\ = 43
+HOME = 199 
+TAB = 15 
+ENTER = 28 
+DELETE = 211 
+BACKSPACE = 14 
+INSERT = 210 
+END = 207 
+PAGEUP = 201 
+PAGEDOWN = 209 
+CAPS = 58 
+A = 30 
+B = 48 
+C = 46 
+D = 32 
+E = 18 
+F = 33 
+G = 34 
+H = 35 
+I = 23 
+J = 36 
+K = 37 
+L = 38 
+M = 50 
+N = 49 
+O = 24 
+P = 25 
+Q = 16 
+R = 19 
+S = 31 
+T = 20 
+U = 22 
+V = 47 
+W = 17 
+X = 45 
+Y = 21 
+Z = 44 
+SHIFTL = 42 
+SHIFTR = 54 
+UP = 200 
+DOWN = 208 
+LEFT = 203 
+RIGHT = 205 
+NUM_0 = 82 
+NUM_1 = 79 
+NUM_2 = 80 
+NUM_3 = 81 
+NUM_4 = 75 
+NUM_5 = 76 
+NUM_6 = 77 
+NUM_7 = 71 
+NUM_8 = 72 
+NUM_9 = 73 
+NUM_+ = 78 
+NUM = 69 
+NUM_/ = 181 
+NUM_* = 55 
+NUM_- = 74 
+NUM_. = 83
+NUM_ENTER = 156 
+CTRLL = 29 
+CTRLR = 157 
+WINL = 219 
+WINR = 220 
+ALTL = 56
+ALTR = 184
+SPACE = 57 
+APP = 221
+*/
