ba25ada7d7bb733ead769afe439a561092ac321a
Michail Nikolaev <michail.nikolaev@gmail.com>
2013-08-09 01:07:51 +0300

    simple serious mode, fixes #75

--- arma3/@task_force_radio/mod.cpp
+++ arma3/@task_force_radio/mod.cpp
@@ -1,5 +1,5 @@
 name = "Task Force Arrowhead Radio";
 picture = "task_force_arrowhead_logo.paa";
 actionName = "Website";
-action = "https://github.com/michail-nikolaev/task-force-arma-3-radio#task-force-arma-3-radio";
+action = "http://michail-nikolaev.github.io/task-force-arma-3-radio/";
 description = "Task Force Arrowhead TeamSpeak Radio Plugin";
