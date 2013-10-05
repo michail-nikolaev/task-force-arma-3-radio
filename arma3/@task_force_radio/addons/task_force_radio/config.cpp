a510be5a838e67d1e968e666c9f70ad7435fbc1e
Michail Nikolaev <michail.nikolaev@gmail.com>
2013-10-05 16:44:36 +0300

    items moved to separated pdo file

--- arma3/@task_force_radio/addons/task_force_radio/config.cpp
+++ arma3/@task_force_radio/addons/task_force_radio/config.cpp
@@ -5,27 +5,13 @@ class CfgPatches
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 1.0;
-		requiredAddons[] = {"Extended_EventHandlers", "A3_UI_F"};
+		requiredAddons[] = {"Extended_EventHandlers", "A3_UI_F", "task_force_radio_items"};
 		author[] = {"[TF]Nkey"};
 		authorUrl = "https://github.com/michail-nikolaev/task-force-arma-3-radio";
-		version = 0.7.0;
-		versionStr = "0.7.0 beta";
-		versionAr[] = {0,7,0};
+		version = 0.8.0;
+		versionStr = "0.8.0";
+		versionAr[] = {0,8,0};
 	};
-};
-
-class CfgVehicles {
-	class Bag_Base;
-	class tf_rt1523g: Bag_Base
-	{
-		displayName = "RT-1523G (ASIP)";
-		descriptionShort = "RT-1523G (ASIP)";
-		picture = "\task_force_radio\rt1523g\rt1523g_icon.paa";
-		model = "\A3\weapons_f\Ammoboxes\bags\Backpack_Small";
-		scope = 2;
-		maximumLoad = 30;
-		mass = 15;
-	};	
 };
 
 class CfgMarkers
