a2f0fbe4b52b5eafd08ef8a89ed3ccd8828f1a05
Garth "L-H" de Wet <garthofhearts@gmail.com>
2014-03-18 00:45:02 +0200

    Added radio crates for West, East and IND with all current radios. (Except backpack radios)

---
+++ arma3/@task_force_radio/addons/task_force_radio_items/crates.hpp
@@ -0,0 +1,63 @@
+class Box_NATO_Support_F;
+
+class TF_NATO_Radio_Crate: Box_NATO_Support_F
+{
+	author = "TFAR";
+	displayName = "TFAR Radio (NATO)";
+	class TransportItems
+	{
+		class _xx_tf_anprc152
+		{
+			count = 40;
+			name = "tf_anprc152";
+		};
+		class _xx_tf_rf7800str
+		{
+			count = 40;
+			name = "tf_rf7800str";
+		};
+	};
+	class TransportMagazines{};
+	class TransportWeapons{};
+};
+
+class Box_EAST_Support_F;
+
+class TF_EAST_Radio_Crate: Box_EAST_Support_F
+{
+	author = "TFAR";
+	displayName = "TFAR Radio (CSAT)";
+	class TransportItems
+	{
+		class _xx_tf_fadak
+		{
+			count = 40;
+			name = "tf_fadak";
+		};
+	};
+	class TransportMagazines{};
+	class TransportWeapons{};
+};
+
+class Box_IND_Support_F;
+
+class TF_IND_Radio_Crate: Box_IND_Support_F
+{
+	author = "TFAR";
+	displayName = "TFAR Radio (AAF)";
+	class TransportItems
+	{
+		class _xx_tf_anprc148jem
+		{
+			count = 40;
+			name = "tf_anprc148jem";
+		};
+		class _xx_tf_anprc154
+		{
+			count = 40;
+			name = "tf_anprc154";
+		};
+	};
+	class TransportMagazines{};
+	class TransportWeapons{};
+};
