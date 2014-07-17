577175cc8ee44aeac57d29a96c46783af5d700c2
Garth "L-H" de Wet <garthofhearts@gmail.com>
2014-07-17 11:21:49 +0200

    Added userconfig option for same frequencies for sw and lr

--- arma3/userconfig/task_force_radio/radio_keys.hpp
+++ arma3/userconfig/task_force_radio/radio_keys.hpp
@@ -1,10 +1,20 @@
 ﻿class task_force_radio_settings {
+	// =================================================
 	// Server side only
+	// =================================================
 	tf_no_auto_long_range_radio = 0;
 	TF_give_personal_radio_to_regular_soldier = 0;
-	
+	tf_same_sw_frequencies_for_side = 0;
+	tf_same_lr_frequencies_for_side = 0;
+	// =================================================
+	// END: Server side only
+	// =================================================
 	// Client side
+	// =================================================
 	tf_default_radioVolume = 7;
+	// =================================================
+	// END: Client side
+	// =================================================
 };
 class task_force_radio_keys {
 
