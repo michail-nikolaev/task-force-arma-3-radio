26b34beb3722060a75a3e9cfd00bd0e5eb8dd272
Michail Nikolaev <michail.nikolaev@gmail.com>
2014-10-16 22:09:09 +0300

    0.9.5 release package

--- arma3/@task_force_radio/addons/task_force_radio/config.cpp
+++ arma3/@task_force_radio/addons/task_force_radio/config.cpp
@@ -8,9 +8,9 @@ class CfgPatches
 		requiredAddons[] = { "CBA_Main", "task_force_radio_items"};
 		author[] = {"[TF]Nkey"};
 		authorUrl = "https://github.com/michail-nikolaev/task-force-arma-3-radio";
-		version = 0.9.4;
-		versionStr = "0.9.4";
-		versionAr[] = {0,9,4};
+		version = 0.9.5;
+		versionStr = "0.9.5";
+		versionAr[] = {0,9,5};
 	};
 };
 
