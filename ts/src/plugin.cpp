99e8f46f1502f14e81de95186820a09c5aafe14e
Michail Nikolaev <michail.nikolaev@gmail.com>
2014-10-18 14:23:17 +0300

    0.9.5.d

--- ts/src/plugin.cpp
+++ ts/src/plugin.cpp
@@ -65,7 +65,7 @@ float distance(TS3_VECTOR from, TS3_VECTOR to)
 	return sqrt(sq(from.x - to.x) + sq(from.y - to.y) + sq(from.z - to.z));
 }
 
-#define PLUGIN_VERSION "0.9.5"
+#define PLUGIN_VERSION "0.9.5d"
 #define CANT_SPEAK_DISTANCE 5
 
 #define PIWIK_URL L"nkey.piwik.pro"
