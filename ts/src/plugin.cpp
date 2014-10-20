0b1f1c5fb50081b0ec50610b48d31a44cd779031
Michail Nikolaev <michail.nikolaev@gmail.com>
2014-10-20 08:59:38 +0300

    work on #737

--- ts/src/plugin.cpp
+++ ts/src/plugin.cpp
@@ -65,7 +65,7 @@ float distance(TS3_VECTOR from, TS3_VECTOR to)
 	return sqrt(sq(from.x - to.x) + sq(from.y - to.y) + sq(from.z - to.z));
 }
 
-#define PLUGIN_VERSION "0.9.5rc"
+#define PLUGIN_VERSION "0.9.5rc2"
 #define CANT_SPEAK_DISTANCE 5
 #define SPEAKER_GAIN 4
 
