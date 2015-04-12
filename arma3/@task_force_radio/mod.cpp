249c07bfe6b1395657251b531430fead557b5eeb
Michail Nikolaev <michail.nikolaev@gmail.com>
2015-04-12 23:31:55 +0300

    fixes #838 (Description in mod.cpp)

--- arma3/@task_force_radio/mod.cpp
+++ arma3/@task_force_radio/mod.cpp
@@ -1,5 +1,14 @@
-name = "Task Force Arrowhead Radio 0.9.7.1";
+name = "Task Force Arrowhead Radio 0.9.7.3";
 picture = "task_force_arrowhead_logo.paa";
 actionName = "Website";
 action = "http://radio.task-force.ru/en";
 description = "Task Force Arma 3 Radio TeamSpeak Plugin";
+logo = "task_force_arrowhead_logo.paa";
+logoOver = "manw_winner_addon_1st_emblem.paa";
+tooltip = "Task Force Arrowhead Radio";
+tooltipOwned = "Task Force Arrowhead Radio";
+overview = "War is not only about moving, waiting and engaging targets. It is also about communication. Task Force Arrowhead Radio provides a way to dramatically increase gameplay realism and atmosphere by seamless integration with TeamSpeak.";
+author = "TFAR Team";
+overviewPicture = "task_force_arrowhead_logo.paa";
+overviewText = "Task Force Arrowhead Radio";
+overviewFootnote = "<br /><br /><t color='#999999'>This content is under APL-SA License.<t />";
