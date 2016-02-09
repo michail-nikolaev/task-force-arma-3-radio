ea50737721af3e0ea1206798b380c10e9e880316
nkey <nkey@yandex-team.ru>
2016-02-09 16:18:50 +0300

    steam workshop changes - step 1

--- arma3/@task_force_radio/addons/task_force_radio/config.cpp
+++ arma3/@task_force_radio/addons/task_force_radio/config.cpp
@@ -16,7 +16,26 @@ class CfgPatches
 
 #include "\task_force_radio\CfgFunctions.h"
 class task_force_radio_settings {
-	#include "\userconfig\task_force_radio\radio_settings.hpp"
+
+// =================================================
+// Server side only
+// =================================================
+tf_no_auto_long_range_radio = 0;
+TF_give_personal_radio_to_regular_soldier = 0;
+TF_give_microdagr_to_soldier = 1;
+tf_same_sw_frequencies_for_side = 0;
+tf_same_lr_frequencies_for_side = 0;
+tf_same_dd_frequencies_for_side = 0;
+// =================================================
+// END: Server side only
+// =================================================
+// Client side
+// =================================================
+tf_default_radioVolume = 7;
+// =================================================
+// END: Client side
+// =================================================
+
 };
 #include "\task_force_radio\description.h"
 #include "\task_force_radio\RscTitles.hpp"
