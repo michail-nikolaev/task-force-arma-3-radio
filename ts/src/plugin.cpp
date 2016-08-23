437f2978d159f8a8501a6a1f45933c9b1a11524d
Matt Smith <psykzz@users.noreply.github.com>
2016-08-23 23:03:24 +0100

    Update plugin version for TS3 Beta

--- ts/src/plugin.cpp
+++ ts/src/plugin.cpp
@@ -44,8 +44,7 @@
 #define MAX_CHANNELS  8
 static float* floatsSample[MAX_CHANNELS];
 
-#define PLUGIN_API_VERSION 20
-//#define PLUGIN_API_VERSION 19
+#define PLUGIN_API_VERSION 21
 
 #define PIPE_NAME L"\\\\.\\pipe\\task_force_radio_pipe"
 //#define PIPE_NAME L"\\\\.\\pipe\\task_force_radio_pipe_debug"
