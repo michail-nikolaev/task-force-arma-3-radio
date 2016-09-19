5fa68d4510babdc770ba7c5745853788620c5281
Dedmen Miller <dedmenmiller@googlemail.com>
2016-09-19 03:34:42 +0200

    Fixes #1109 (#1112)
    Updated test functions in main() to newer syntax
    Changed NamedPipeTransact to be Async
    Added Timeout to NamedPipeTransact

--- arma3/@task_force_radio/task_force_radio_pipe/dllmain.cpp
+++ arma3/@task_force_radio/task_force_radio_pipe/dllmain.cpp
@@ -2,6 +2,7 @@
 #include "stdafx.h"
 
 HANDLE pipe = INVALID_HANDLE_VALUE;
+HANDLE waitForDataEvent = INVALID_HANDLE_VALUE;//#Dedmen i dont know why these are defined here. They are not needed here.
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -36,10 +37,11 @@ int main(int argc, char * argv [])
 		DWORD ticks = GetTickCount();
 		for (int q = 0; q < 1000; q++) 
 		{
-			RVExtension(output, TEST_OUTPUT_SIZE, "POS@[TF]Nkey@0.5@9.3@123.5003@236");
-			RVExtension(output, TEST_OUTPUT_SIZE, "POS@[TF]Nkey2@5@-0.7@123.5003@236");
-			RVExtension(output, TEST_OUTPUT_SIZE, "TANGENT@PRESSED");
-			RVExtension(output, TEST_OUTPUT_SIZE, "TANGENT@RELEASED");			
+			RVExtension(output, TEST_OUTPUT_SIZE, "VERSION\ttest\test_channel\ttest_password");
+			RVExtension(output, TEST_OUTPUT_SIZE, "POS\t[TF]Nkey\t0.5\t9.3\t123.5003\t236");
+			RVExtension(output, TEST_OUTPUT_SIZE, "POS\t[TF]Nkey2\t5\t-0.7\t123.5003\t236");
+			RVExtension(output, TEST_OUTPUT_SIZE, "TANGENT\tPRESSED");
+			RVExtension(output, TEST_OUTPUT_SIZE, "TANGENT\tRELEASED");			
 		}
 		printf("%f\n", (float)(GetTickCount() - ticks) / 4000.0);
 		//Sleep(200);
