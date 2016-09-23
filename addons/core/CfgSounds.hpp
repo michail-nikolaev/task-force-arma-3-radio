80379e520fba2319b44362cbe3895bd9eb0e91f7
Ignacio Muñoz Fernandez <nmunozfernandez@gmail.com>
2016-09-23 18:42:12 +0200

    Moved Sounds

---
+++ addons/core/CfgSounds.hpp
@@ -0,0 +1,16 @@
+class CfgSounds
+{
+    class TFAR_rotatorPush
+    {
+        name = "TFAR - Rotator Switch (Push)";
+        sound[] = {PATHTOF(sounds\hardPush.wss), 0.5, 1};
+        titles[] = {};
+    };
+
+    class TFAR_rotatorClick
+    {
+        name = "TFAR - Rotator Switch (Click)";
+        sound[] = {"\A3\ui_f\data\sound\RscButton\soundEscape.wss", 0.5, 1};
+        titles[] = {};
+    };
+};
--- addons/core/config.cpp
+++ addons/core/config.cpp
@@ -30,3 +30,4 @@ class CfgMods {
 };
 
 #include "CfgEventHandlers.hpp"
+#include "CfgSounds.hpp"
