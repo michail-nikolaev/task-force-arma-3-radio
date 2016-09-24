beec0c3bb6d96f8e1ab4cfa37e15b2f21e425c4b
dedmen <dedmen@dedmen.de>
2016-09-24 18:28:03 +0200

    Fixed creating class twice

--- addons/core/uiDefines.hpp
+++ addons/core/uiDefines.hpp
@@ -1,7 +1,12 @@
-#include "RadioDialogControls.hpp"
 #define SW_EDIT	1400
 #define SW_CHANNEL	1604
 #define ID_ADDITIONAL 12345
 #define ID_SPEAKERS 123456
 #define LR_EDIT	1410
 #define LR_CHANNEL	1411
+/// Base Classes
+class RscBackPicture;
+class RscEditLCD;
+class HiddenButton;
+class HiddenRotator;
+class HiddenFlip;
