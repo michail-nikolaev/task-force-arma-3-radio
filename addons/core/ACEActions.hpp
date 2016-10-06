2e4aca57f88dd074f34e4ad98223db067f2b15f1
Garth 'L-H' de Wet <garthofhearts@gmail.com>
2016-10-06 21:44:15 +0200

    Tab -> 4 Space
    Also removed debug command.

--- addons/core/ACEActions.hpp
+++ addons/core/ACEActions.hpp
@@ -1,13 +1,13 @@
 class Man;
 class CAManBase: Man {
-	class ACE_SelfActions {
-		class TFAR_Radio {
-			displayName = "Radios";
-			condition = "call TFAR_fnc_haveSWRadio";
-			exceptions[] = {"isNotSwimming"};
-			statement = "";
-			icon = "";
-			insertChildren = "[_player] call TFAR_fnc_addRadiosToACE";
-		};
-	};
+    class ACE_SelfActions {
+        class TFAR_Radio {
+            displayName = "Radios";
+            condition = "call TFAR_fnc_haveSWRadio";
+            exceptions[] = {"isNotSwimming"};
+            statement = "";
+            icon = "";
+            insertChildren = "[_player] call TFAR_fnc_addRadiosToACE";
+        };
+    };
 };
