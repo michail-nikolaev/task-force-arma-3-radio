88e817ab8cef7fbb471a42d3d170613ac204afe5
dedmen <dedmen@dedmen.de>
2017-03-03 13:06:41 +0100

    Give bwmod mr3000 bluefor radio code

--- addons/backpacks/mr3000/CfgVehicles.hpp
+++ addons/backpacks/mr3000/CfgVehicles.hpp
@@ -28,6 +28,7 @@ class TFAR_mr3000_bwmod: TFAR_mr3000 {
     descriptionShort = "MR3000 BWMOD long range radio 20km";//#Stringtable
     hiddenSelections[] = {"camo"};
     hiddenSelectionsTextures[] = {QPATHTOF(models\data\clf_nicecomm2_bwmod_co.paa)};
+	tf_encryptionCode = "tf_west_radio_code";
 };
 HIDDEN_CLASS(tf_mr3000_bwmod : TFAR_mr3000_bwmod); //#Deprecated dummy class for backwards compat
 class TFAR_mr3000_bwmod_tropen: TFAR_mr3000_bwmod {
