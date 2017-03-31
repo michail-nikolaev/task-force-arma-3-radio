65b19ebd5dbda7e2a6e432c50becf48bac8afa90
dedmen <dedmen@dedmen.de>
2017-03-31 18:13:19 +0200

    Moved Radios into Equipment submenu

--- addons/core/ACEActions.hpp
+++ addons/core/ACEActions.hpp
@@ -1,13 +1,15 @@
 class Man;
 class CAManBase: Man {
     class ACE_SelfActions {
-        class TFAR_Radio {
-            displayName = "Radios";
-            condition = "call TFAR_fnc_haveSWRadio";
-            exceptions[] = {"isNotSwimming"};
-            statement = "";
-            icon = QPATHTOF(ui\ACE_Interaction_Radio_Icon.paa);
-            insertChildren = "[_player] call TFAR_fnc_addRadiosToACE";
+        class ACE_Equipment {
+            class TFAR_Radio {
+                displayName = "Radios";
+                condition = "call TFAR_fnc_haveSWRadio";
+                exceptions[] = {"isNotSwimming"};
+                statement = "";
+                icon = QPATHTOF(ui\ACE_Interaction_Radio_Icon.paa);
+                insertChildren = "[_player] call TFAR_fnc_addRadiosToACE";
+            };
         };
     };
 };
