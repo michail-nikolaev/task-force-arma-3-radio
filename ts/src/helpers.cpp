547b3e108e330399d062e4aa8fc829f66023caf9
dedmen <dedmen@dedmen.de>
2017-04-21 14:13:21 +0200

    Added Vector CrossProduct function
    Use SSE Intrinsics in Gain(Sound) Functions if available
    Fixed 3D Positioning math errors

--- ts/src/helpers.cpp
+++ ts/src/helpers.cpp
@@ -9,6 +9,7 @@
 #include "task_force_radio.hpp"
 #include <bitset>
 #include <public_errors.h>
+#define CAN_USE_SSE_ON(x) (IsProcessorFeaturePresent(PF_XMMI64_INSTRUCTIONS_AVAILABLE) && (reinterpret_cast<uintptr_t>(x) % 16 == 0))
 
 void helpers::applyGain(short * samples, size_t sampleCount, int channels, float directTalkingVolume) {
     if (directTalkingVolume == 0.0f) {
@@ -17,7 +18,19 @@ void helpers::applyGain(short * samples, size_t sampleCount, int channels, float
     }
     if (directTalkingVolume == 1.0f) //no change in gain
         return;
-    for (size_t i = 0; i < sampleCount * channels; i++) samples[i] = static_cast<short>(samples[i] * directTalkingVolume);
+
+
+
+    size_t leftOver = 0;
+    if (CAN_USE_SSE_ON(samples)) {
+        auto leftOver = (sampleCount * channels) % 8;
+        __m128 xmm3;
+        float multiplier[4] = { directTalkingVolume ,directTalkingVolume , directTalkingVolume, directTalkingVolume };//This is limiting to 4 channels max. But If we implement surround sound we don't really need a center
+        xmm3 = _mm_loadu_ps(multiplier);
+        helpers::shortFloatMultEx(samples, sampleCount * channels, xmm3);
+    }
+
+    for (size_t i = sampleCount * channels - leftOver; i < sampleCount * channels; i++) samples[i] = static_cast<short>(samples[i] * directTalkingVolume);
 }
 
 constexpr float DegToRad(float deg) {
@@ -31,7 +44,15 @@ void helpers::applyILD(short * samples, size_t sampleCount, int channels, Direct
         float gainFrontRight = AngleDegrees(21.5f).toRadians() * dir.cosine() + 0.625f;
         //Use https://msdn.microsoft.com/en-us/library/windows/desktop/ee415798(v=vs.85).aspx for more than 2 channels
 
-        for (size_t i = 0; i < sampleCount * channels; i += channels) {
+        size_t leftOver = sampleCount * channels;
+        if (CAN_USE_SSE_ON(samples)) {
+            auto leftOver = (sampleCount * channels) % 8;
+            __m128 xmm3;
+            float multiplier[4] = { gainFrontLeft ,gainFrontRight , gainFrontLeft, gainFrontRight };//This is limiting to 4 channels max. But If we implement surround sound we don't really need a center
+            xmm3 = _mm_loadu_ps(multiplier);
+            helpers::shortFloatMultEx(samples, sampleCount * channels, xmm3);
+        }
+        for (size_t i = sampleCount * channels - leftOver; i < sampleCount * channels; i += channels) {
             samples[i] = static_cast<short>(samples[i] * gainFrontLeft);
             samples[i + 1] = static_cast<short>(samples[i + 1] * gainFrontRight);
         }
@@ -57,16 +78,27 @@ void helpers::applyILD(short * samples, size_t sampleCount, int channels, Positi
     //#X3DAudio fix up vector problem
     X3DAUDIO_LISTENER listener{};
     std::tie(listener.OrientFront.x, listener.OrientFront.y, listener.OrientFront.z) = myViewDirection.get();
-    listener.OrientFront.y = -listener.OrientFront.y;
-    listener.OrientFront.x = -listener.OrientFront.x;
-    std::tie(listener.OrientTop.x, listener.OrientTop.y, listener.OrientTop.z) = Direction3D(-std::get<0>(myViewDirection.get()), -std::get<1>(myViewDirection.get()), 1.f).get();
+    listener.OrientFront.x = -listener.OrientFront.x;//East and West are mixed up
+    listener.OrientFront.y = -listener.OrientFront.y;//North and South are mixed up   
+    //listener.OrientFront.z = 0;
+
+    auto myRightVector = myViewDirection.crossProduct({ 0,0,1 });
+    auto myUpVector = myRightVector.crossProduct(myViewDirection);
+   
+    //listener.OrientFront = { 0,-1,0 };
+    
+    
+    
+    std::tie(listener.OrientTop.x, listener.OrientTop.y, listener.OrientTop.z) = myUpVector.get();// Direction3D(0/*-std::get<0>(myViewDirection.get())*/, -std::get<2>(myViewDirection.get()), std::get<1>(myViewDirection.get())).get();
+    //listener.OrientTop = { 0,0,1 };
     std::tie(listener.Position.x, listener.Position.y, listener.Position.z) = myPosition.get();
     listener.pCone = NULL;
 
     X3DAUDIO_EMITTER emitter{};
-
+    emitter.pCone = NULL;
     std::tie(emitter.Position.x, emitter.Position.y, emitter.Position.z) = emitterPosition.get();
     std::tie(emitter.OrientFront.x, emitter.OrientFront.y, emitter.OrientFront.z) = emitterViewDirection.get();
+    //emitter.OrientFront = { 0,1,0 };
     //std::tie(emitter.OrientTop.x, emitter.OrientTop.y, emitter.OrientTop.z) = emitterRotation.up.get();
     emitter.ChannelCount = 1;
     emitter.CurveDistanceScaler = 1.f;
@@ -98,10 +130,58 @@ void helpers::applyILD(short * samples, size_t sampleCount, int channels, Positi
     gainFrontLeft *= mult; //make sure left+right = 1.0 else it would decrease overall volume which we don't want
     gainFrontRight *= mult;
 
-
-    for (size_t i = 0; i < sampleCount * channels; i += channels) {
+    size_t leftOver = sampleCount * channels;
+    if (CAN_USE_SSE_ON(samples)) { //Can use SSE and memory is correctly aligned
+        auto leftOver = (sampleCount * channels) % 8;
+        __m128 xmm3;
+        float multiplier[4] = { gainFrontLeft ,gainFrontRight , gainFrontLeft, gainFrontRight };//This is limiting to 4 channels max. But If we implement surround sound we don't really need a center
+        xmm3 = _mm_loadu_ps(multiplier);
+        helpers::shortFloatMultEx(samples, sampleCount * channels, xmm3);
+    }
+    for (size_t i = sampleCount * channels - leftOver; i < sampleCount * channels; i += channels) {
         samples[i] = static_cast<short>(samples[i] * gainFrontLeft);
         samples[i + 1] = static_cast<short>(samples[i + 1] * gainFrontRight);
+    }
+}
+
+void helpers::shortFloatMultEx(short * data, size_t elementCount, __m128 multPack) {//#TODO use in gain and ILD for ILD multPack is {left,right,left,right}
+
+    /*
+    Thanks GCC!
+    https://godbolt.org/#g:!((g:!((g:!((h:codeEditor,i:(j:1,source:'%23include+%3Calgorithm%3E%0Ashort+square(int+num)+%7B%0A++++short+samples%5B4096%5D%3B%0A%0A++++for+(size_t+i+%3D+0%3B+i+%3C+4096%3B+i%2B%2B)+samples%5Bi%5D+%3D+static_cast%3Cshort%3E(samples%5Bi%5D+*+0.999f)%3B%0A+++//std::transform(samples,+samples%2B4096,+samples,+%5B%5D(auto+samp)+%7Breturn+samp+*+0.999f%3B+%7D)%3B%0A++++return+samples%5B777%5D%3B%0A%7D'),l:'5',n:'0',o:'C%2B%2B+source+%231',t:'0')),k:50,l:'4',n:'0',o:'',s:0,t:'0'),(g:!((h:compiler,i:(compiler:g7snapshot,filters:(b:'0',commentOnly:'0',directives:'0',intel:'0'),options:'-O3',source:1),l:'5',n:'0',o:'x86-64+gcc+7+(snapshot)+(Editor+%231,+Compiler+%231)',t:'0')),k:50,l:'4',n:'0',o:'',s:0,t:'0')),l:'2',n:'0',o:'',t:'0')),version:4
+    */
+
+#ifdef _DEBUG
+    if (!(CAN_USE_SSE_ON(data)) || (elementCount % 8)) __debugbreak();
+#endif
+
+    for (size_t i = 0; i < elementCount; i += 8) {
+        __m128i xmm0;
+        __m128i xmm1;
+        __m128i xmm2;
+        __m128i xmm4{ 0 };
+
+
+        _mm_store_si128(&xmm2, xmm4);
+        xmm1 = _mm_load_si128((__m128i*) (data + i));
+        xmm2 = _mm_cmpgt_epi16(xmm2, xmm1);
+        _mm_store_si128(&xmm0, xmm1);
+        xmm1 = _mm_unpackhi_epi16(xmm1, xmm2);
+        xmm0 = _mm_unpacklo_epi16(xmm0, xmm2);
+        auto multPack1 = _mm_cvtepi32_ps(xmm1);
+        auto multPack2 = _mm_cvtepi32_ps(xmm0);
+        multPack1 = _mm_mul_ps(multPack1, multPack);
+        multPack2 = _mm_mul_ps(multPack2, multPack);
+        xmm1 = _mm_cvttps_epi32(multPack1);
+        xmm0 = _mm_cvttps_epi32(multPack2);
+        _mm_store_si128(&xmm2, xmm0);
+        xmm0 = _mm_unpacklo_epi16(xmm0, xmm1);
+        xmm2 = _mm_unpackhi_epi16(xmm2, xmm1);
+        _mm_store_si128(&xmm1, xmm0);
+        xmm0 = _mm_unpacklo_epi16(xmm0, xmm2);
+        xmm1 = _mm_unpackhi_epi16(xmm1, xmm2);
+        xmm0 = _mm_unpacklo_epi16(xmm0, xmm1);
+        _mm_store_si128((__m128i*) (data + i), xmm0);
     }
 }
 
@@ -154,8 +234,8 @@ std::vector<boost::string_ref>& helpers::split(boost::string_ref s, char delim, 
             pos = length;
         }
 
-       // if (pos != lastPos /*|| !trimEmpty*/)
-            elems.push_back(s.substr(lastPos, lastPos + pos - lastPos));
+        // if (pos != lastPos /*|| !trimEmpty*/)
+        elems.push_back(s.substr(lastPos, lastPos + pos - lastPos));
 
         lastPos += pos + 1;
     }
@@ -180,7 +260,7 @@ short* helpers::allocatePool(int sampleCount, int channels, short* samples) {
     return result;
 }
 
-void helpers::mix(short* to, short* from, int sampleCount, int channels) {
+void helpers::mix(short* to, short* from, int sampleCount, int channels) {//#TODO SSE2 implementation
     for (int q = 0; q < sampleCount * channels; q++) {
         int sum = to[q] + from[q];
 
