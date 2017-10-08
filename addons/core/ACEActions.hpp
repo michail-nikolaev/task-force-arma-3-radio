dccae9c1de0481909d8138c791804841112054de
Dedmen Miller <dedmen@users.noreply.github.com>
2017-10-08 13:16:49 +0200

    Add ACE support (#1144) (#1315)
    * Add configs to support ACE self interact
    
    Handheld radios that the player has  will show under a Radio action within the self interact menu.
    
    Currently it only supports opening the radio interface.
    
    * Tab -> 4 Space
    
    Also removed debug command.
    
    * Fixed indentation of CfgVehicles
    
    * Add support for ACE MicroDAGR as radio programmer
    
    * CRLF -> LF
    
    * Fix stuff
    
    * Unfix stuff
    
    * Fix stuff again
    
    * Added Radio Icon
    
    * Moved Radios into Equipment submenu
    
    * Add lower/Raise headset action
    
    * Added LR Radios to Radios Menu
    
    * Preparations for using Remote LR Radios
    
    * Fixed Additional hotkeys block vanilla functionality
    
    * CBA submodule update
    
    * Disable remote LR ACE action

---
+++ addons/core/ACEActions.hpp
@@ -0,0 +1,46 @@
+class Man;
+class CAManBase: Man {
+    class ACE_SelfActions {
+        class ACE_Equipment {
+            class TFAR_Radio {
+                displayName = "Radios";
+                condition = "call TFAR_fnc_haveSWRadio";
+                exceptions[] = {};
+                statement = "";
+                icon = QPATHTOF(ui\ACE_Interaction_Radio_Icon.paa);
+                insertChildren = "[_player] call TFAR_fnc_addRadiosToACE";
+            };
+            class TFAR_LowerHeadset {
+                displayName = "Lower Headset";
+                condition = "(!(missionNamespace getVariable ['TFAR_core_isHeadsetLowered',false])) && (call TFAR_fnc_haveSWRadio || call TFAR_fnc_haveLRRadio)";
+                //exceptions[] = {};
+                statement = "true call TFAR_fnc_setHeadsetLowered;";
+                //showDisabled = 0;
+                icon = "";
+            };
+            class TFAR_RaiseHeadset {
+                displayName = "Raise Headset";
+                condition = "(missionNamespace getVariable ['TFAR_core_isHeadsetLowered',false]) && (call TFAR_fnc_haveSWRadio || call TFAR_fnc_haveLRRadio)";
+                //exceptions[] = {};
+                statement = "false call TFAR_fnc_setHeadsetLowered;";
+                //showDisabled = 0;
+                icon = "";
+            };
+        };
+    };
+    //class ACE_Actions {
+    //    class TFAR_Radios {
+    //        displayName = "Radios";
+    //        condition = "count (_target call TFAR_fnc_lrRadiosList) > 0";
+    //        insertChildren = "[_target,true] call TFAR_fnc_addRadiosToACE";
+    //        class TFAR_UseLRRadio {
+    //            displayName = "Radios";
+    //            condition = "true";
+    //            exceptions[] = {};
+    //            statement = "";
+    //            icon = "";
+    //            insertChildren = "[_target] call TFAR_fnc_addRadiosToACE";
+    //        };
+    //    };
+    //};
+};
