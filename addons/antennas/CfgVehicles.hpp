cfe4640994d6db701f90a20343ae6f919d600217
Thomas Müller <Dorbedo@t-online.de>
2017-10-24 19:24:37 +0200

    Editor/Curator stuff

--- addons/antennas/CfgVehicles.hpp
+++ addons/antennas/CfgVehicles.hpp
@@ -2,6 +2,11 @@ class CfgVehicles {
     class Land_Communication_F;
 
     class TFAR_Land_Communication_F : Land_Communication_F {
+        scope = PUBLIC;
+        scopeCurator = PUBLIC;
+        author = QUOTE(AUTHORS);
+        category = "TFAR";
+        editorCategory = "TFAR";
         displayName = CSTRING(TowerName);
     };
 };
