01016771bdad293c39c8a1e09ab0aec687738fd1
Dorbedo <Dorbedo@t-online.de>
2017-11-04 22:32:34 +0100

    Convert AI-Hearing into CBA-Setting (#1326)
    * initial commit
    
    * debug note removed
    
    * not needed here
    
    * removing/adding the EH
    
    * fixing
    
    * disable debug

--- addons/ai_hearing/script_component.hpp
+++ addons/ai_hearing/script_component.hpp
@@ -1,12 +1,18 @@
 #define COMPONENT ai_hearing
 #include "\z\tfar\addons\core\script_mod.hpp"
 
-#ifdef DEBUG_ENABLED_CORE
+#ifdef DEBUG_ENABLED_AI_HEARING
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_CORE
+#ifdef DEBUG_SETTINGS_AI_HEARING
     #define DEBUG_SETTINGS DEBUG_SETTINGS_MAIN
 #endif
 
 #include "\z\tfar\addons\core\script_macros.hpp"
+
+#ifdef PREP
+    #undef PREP
+#endif
+
+#define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
