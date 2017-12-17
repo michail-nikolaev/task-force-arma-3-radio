631e29d64dcb25cd68bf448df2b6fa41ed056264
Thomas Müller <Dorbedo@t-online.de>
2017-12-17 21:38:01 +0100

    macros fixed

--- addons/handhelds/radio_ids.hpp
+++ addons/handhelds/radio_ids.hpp
@@ -1,4 +1,4 @@
-#define TF_RADIO_ID_full(baseClass,displayNameBase,index) class baseClass##_##index : baseClass \
+#define TF_RADIO_ID_full(baseClass,displayNameBase,index) class DOUBLES(baseClass,index) : baseClass \
     { \
         displayName = 'displayNameBase index'; \
         scope = 1; \
@@ -9,34 +9,36 @@
     };
 
 
-#define TF_RADIO_IDS_1(baseClass,displayNameBase,index,one) class baseClass##_##index : baseClass##_##one \
+#define TF_RADIO_IDS_1(baseClass,displayNameBase,index,one) class DOUBLES(baseClass,index) : DOUBLES(baseClass,one) \
 { \
     displayName = 'displayNameBase index'; \
 };
 
+#define CREATE_INDEX(VAR1,VAR2) VAR1##VAR2
+
 #define TF_RADIO_IDS_10(baseClass,displayNameBase,index10) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##0,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##1,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##2,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##3,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##4,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##5,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##6,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##7,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##8,1) \
-    TF_RADIO_IDS_1(baseClass,displayNameBase,index10##9,1)
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,0),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,1),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,2),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,3),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,4),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,5),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,6),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,7),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,8),1) \
+    TF_RADIO_IDS_1(baseClass,displayNameBase,CREATE_INDEX(index10,9),1)
 
 #define TF_RADIO_IDS_100(baseClass,displayNameBase,index100) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##0) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##1) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##2) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##3) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##4) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##5) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##6) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##7) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##8) \
-    TF_RADIO_IDS_10(baseClass,displayNameBase,index100##9)
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,0)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,1)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,2)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,3)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,4)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,5)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,6)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,7)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,8)) \
+    TF_RADIO_IDS_10(baseClass,displayNameBase,CREATE_INDEX(index100,9))
 
 
 #define TF_RADIO_IDS_999(baseClass,displayNameBase) \
