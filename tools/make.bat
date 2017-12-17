0e83cb306c3784614f2c7ab0051732a8252c5a7b
Thomas Müller <Dorbedo@t-online.de>
2017-12-17 23:21:56 +0100

    bat changed

---
+++ tools/make.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set armake=armake_w64.exe) else (set armake=armake_w32.exe)
+
+for /d %%f in (..\addons\*) do (
+    set folder=%%f
+    set name=!folder:..\addons\=!
+    echo   PBO  ..\addons\tfar_!name!.pbo
+    !armake! build -i ..\include -w unquoted-string -w redefinition-wo-undef -f !folder! ..\addons\tfar_!name!.pbo
+)
+
+pause
