062915072bf3e4d22d74c2acc7e657a68d0867c3
Thomas Müller <Dorbedo@t-online.de>
2018-05-13 09:29:16 +0200

    revert build.py

--- tools/build.py
+++ tools/build.py
@@ -3,14 +3,10 @@
 import os
 import sys
 import subprocess
-import struct
-import shutil
-import platform
 
 ######## GLOBALS #########
 MAINPREFIX = "z"
 PREFIX = "tfar_"
-USEARMAKE = True
 ##########################
 
 def mod_time(path):
@@ -23,25 +19,15 @@ def mod_time(path):
 
 
 def check_for_changes(addonspath, module):
-    if not os.path.exists(os.path.join(addonspath, "{}{}.pbo".format(PREFIX, module))):
+    if not os.path.exists(os.path.join(addonspath, "{}{}.pbo".format(PREFIX,module))):
         return True
-    return mod_time(os.path.join(addonspath, module)) > mod_time(os.path.join(addonspath, \
-        "{}{}.pbo".format(PREFIX, module)))
+    return mod_time(os.path.join(addonspath, module)) > mod_time(os.path.join(addonspath, "{}{}.pbo".format(PREFIX,module)))
 
 def check_for_obsolete_pbos(addonspath, file):
     module = file[len(PREFIX):-4]
     if not os.path.exists(os.path.join(addonspath, module)):
         return True
     return False
-
-def check_workdrive(path, addonspath):
-    workdrivepath = os.path.normpath("{}/{}/{}/".format(path, MAINPREFIX, PREFIX.rstrip("_")))
-    if not os.path.exists(workdrivepath):
-        os.makedirs(workdrivepath)
-    workdrivepath = os.path.normpath(workdrivepath + "/addons")
-    if not os.path.exists(os.path.normpath(workdrivepath + "/core")):
-        os.symlink(addonspath, workdrivepath)
-    return True
 
 def main():
     print("""
@@ -53,13 +39,6 @@ def main():
     scriptpath = os.path.realpath(__file__)
     projectpath = os.path.dirname(os.path.dirname(scriptpath))
     addonspath = os.path.join(projectpath, "addons")
-    try:
-        check_workdrive(os.path.normpath(projectpath + "/include"), addonspath)
-    except:
-        print("The virtual workdrive could not be checked.")
-        print("Please link '/addons' to 'include/{}/{}/addons'".\
-            format(MAINPREFIX, PREFIX.rstrip("_")))
-        print("or run this script as administrator")
 
     os.chdir(addonspath)
 
@@ -74,12 +53,6 @@ def main():
                 removed += 1
                 print("  Removing obsolete file => " + file)
                 os.remove(file)
-
-    if platform.system() == "Windows":
-        path_armake = os.path.normpath(projectpath + "/tools/armake_w64.exe")
-    else:
-        path_armake = "armake"
-
     print("")
 
     for p in os.listdir(addonspath):
@@ -96,11 +69,13 @@ def main():
         print("# Making {} ...".format(p))
 
         try:
-            command = path_armake + " build -i " + os.path.normpath(projectpath + "/include") + \
-                " -w unquoted-string -w redefinition-wo-undef -f " + \
-                os.path.normpath(addonspath + "/" + p) + " " + \
-                os.path.normpath(addonspath + "/" + PREFIX + p + ".pbo")
-            subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+            subprocess.check_output([
+                "makepbo",
+                "-NUP",
+                "-@={}\\{}\\addons\\{}".format(MAINPREFIX,PREFIX.rstrip("_"),p),
+                p,
+                "{}{}.pbo".format(PREFIX,p)
+            ], stderr=subprocess.STDOUT)
         except:
             failed += 1
             print("  Failed to make {}.".format(p))
@@ -109,10 +84,8 @@ def main():
             print("  Successfully made {}.".format(p))
 
     print("\n# Done.")
-    print("  Made {}, skipped {}, removed {}, failed to make {}.".format(made, \
-        skipped, removed, failed))
+    print("  Made {}, skipped {}, removed {}, failed to make {}.".format(made, skipped, removed, failed))
 
-    return failed
 
 if __name__ == "__main__":
     sys.exit(main())
