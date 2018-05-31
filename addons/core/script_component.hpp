79617266075ce59080c65ab17ec93539eba97e29
Dedmen Miller <dedmen@dedmen.de>
2018-05-31 19:37:26 +0200

    Debug UI can change center target

--- addons/core/script_component.hpp
+++ addons/core/script_component.hpp
@@ -2,7 +2,7 @@
 
 #include "\z\tfar\addons\core\script_mod.hpp"
 
-#define DEBUG_PROFCONTEXT
+//#define DEBUG_PROFCONTEXT
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define CBA_DEBUG_SYNCHRONOUS
