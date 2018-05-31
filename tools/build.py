cd4de3d933a6142856979bf8eb5eadbe82e59dbb
Thomas Müller <Dorbedo@t-online.de>
2018-05-31 18:34:46 +0200

    update build.py

--- tools/build.py
+++ tools/build.py
@@ -6,12 +6,59 @@ import subprocess
 import struct
 import shutil
 import platform
+import string
+import urllib.request
+import zipfile, urllib.request
+import re
+from distutils.version import LooseVersion, StrictVersion
+
+#>>> version.parse("2.3.1") < version.parse("10.1.2")
 
 ######## GLOBALS #########
 MAINPREFIX = "z"
 PREFIX = "tfar_"
-USEARMAKE = True
+AUTOUPDATEARMAKE = True
+RELEASEPAGE = "https://github.com/KoffeinFlummi/armake/releases.html"
+PATH_ARMAKE = ""
 ##########################
+
+def armake_latest_version():
+    page = urllib.request.urlopen(RELEASEPAGE)
+    bytearray = page.read()
+    page.close()
+    htmlstring = bytearray.decode("utf8")
+    version = re.search("<a href="".*?/releases/download/v(.*?)/.*?.zip"".*?>", htmlstring)
+    return version.group(1)
+
+def armake_current_version():
+    if AUTOUPDATEARMAKE: return "0.0.0"
+    try:
+        command = PATH_ARMAKE + " --version "
+        version = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True).decode("utf8").replace("v", "")
+        return version
+    except:
+        return "0.0.0"
+
+def armake_download():
+    version = armake_latest_version()
+    latestversion = urllib.request.urlopen("https://github.com/KoffeinFlummi/armake/releases/download/v" + version + "/armake_v" + version + ".zip")
+    tempfile = os.path.dirname(os.path.realpath(__file__)) + "\\armake.zip"
+
+    with open(tempfile, "wb") as download:
+        download.write(latestversion.read())
+        download.close()
+
+    with zipfile.ZipFile(tempfile) as zip_file:
+        for member in zip_file.namelist():
+            filename = os.path.basename(member)
+            if not filename:
+                continue
+            source = zip_file.open(member)
+            target = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), filename), "wb")
+            with source, target:
+                shutil.copyfileobj(source, target)
+
+
 
 def mod_time(path):
     if not os.path.isdir(path):
@@ -45,6 +92,16 @@ def main():
     projectpath = os.path.dirname(os.path.dirname(scriptpath))
     addonspath = os.path.join(projectpath, "addons")
 
+    global PATH_ARMAKE
+    if platform.system() == "Windows":
+        PATH_ARMAKE = os.path.normpath(projectpath + "/tools/armake_w64.exe")
+    else:
+        PATH_ARMAKE = "armake"
+
+    if (LooseVersion(armake_current_version()) < LooseVersion(armake_latest_version())):
+        print("Updating armake")
+        armake_download()
+
     os.chdir(addonspath)
 
     made = 0
@@ -59,10 +116,6 @@ def main():
                 print("  Removing obsolete file => " + file)
                 os.remove(file)
 
-    if platform.system() == "Windows":
-        path_armake = os.path.normpath(projectpath + "/tools/armake_w64.exe")
-    else:
-        path_armake = "armake"
 
     print("")
 
