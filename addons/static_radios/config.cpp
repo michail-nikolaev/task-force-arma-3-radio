120022fff45e5aab5c72d807bb851276a17b075a
Dedmen Miller <dedmen@dedmen.de>
2019-03-03 14:11:32 +0100

    Compile staticRadios functions at preStart

--- addons/static_radios/config.cpp
+++ addons/static_radios/config.cpp
@@ -14,6 +14,13 @@ class CfgPatches {
     };
 };
 
+
+class Extended_PreStart_EventHandlers {
+    class ADDON {
+        init = QUOTE(call COMPILE_FILE(XEH_preStart));
+    };
+};
+
 class Extended_PreInit_EventHandlers {
     class ADDON {
         init = QUOTE(call COMPILE_FILE(XEH_preInit));
