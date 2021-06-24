dff5113e2d8e1c673aaff34e1a037f645d25767e
Saborknight <support@saborknight.com>
2021-06-24 17:56:35 +1000

    Cleaned up - Redundancies in Intercom Phone addon
    What
    =================
    Mostly just redundant default parameters of `nil` and such.
    
    Issue ID: N/A

--- addons/intercom_phone/ACEActions.hpp
+++ addons/intercom_phone/ACEActions.hpp
@@ -5,7 +5,7 @@ class child : parent { \
             class TFAR_Intercom_Phone_Activate { \
                 displayName = CSTRING(PICKUP); \
                 distance = 4; \
-                condition = "isNil {_player getVariable ""TFAR_vehicleIDOverride""} && ([(typeOf _target), ""TFAR_hasIntercom"", 0] call TFAR_fnc_getVehicleConfigProperty) > 0 && isNil {_target getVariable [""TFAR_IntercomPhoneSpeaker"", nil]}"; \
+                condition = "isNil {_player getVariable 'TFAR_vehicleIDOverride'} && ([(typeOf _target), 'TFAR_hasIntercom', 0] call TFAR_fnc_getVehicleConfigProperty) > 0 && isNil {_target getVariable 'TFAR_IntercomPhoneSpeaker'}"; \
                 exceptions[] = {}; \
                 statement = "call TFAR_intercom_phone_fnc_activate;"; \
                 icon = QPATHTOF(ui\tfar_ace_interaction_intercom_phone.paa); \
@@ -13,7 +13,7 @@ class child : parent { \
             class TFAR_Intercom_Phone_Deactivate { \
                 displayName = CSTRING(PUT_AWAY); \
                 distance = 4; \
-                condition = "!isNil {_player getVariable ""TFAR_vehicleIDOverride""} && ([(typeOf _target), ""TFAR_hasIntercom"", 0] call TFAR_fnc_getVehicleConfigProperty) > 0"; \
+                condition = "!isNil {_player getVariable 'TFAR_vehicleIDOverride'} && ([(typeOf _target), 'TFAR_hasIntercom', 0] call TFAR_fnc_getVehicleConfigProperty) > 0"; \
                 exceptions[] = {}; \
                 statement = "call TFAR_intercom_phone_fnc_deactivate;"; \
                 icon = QPATHTOF(ui\tfar_ace_interaction_intercom_phone.paa); \
@@ -21,7 +21,7 @@ class child : parent { \
             class TFAR_Intercom_Phone_Busy { \
                 displayName = CSTRING(BUSY); \
                 distance = 4; \
-                condition = "isNil {_player getVariable ""TFAR_vehicleIDOverride""} && ([(typeOf _target), ""TFAR_hasIntercom"", 0] call TFAR_fnc_getVehicleConfigProperty) > 0 && !isNil {_target getVariable [""TFAR_IntercomPhoneSpeaker"", nil]}"; \
+                condition = "isNil {_player getVariable 'TFAR_vehicleIDOverride'} && ([(typeOf _target), 'TFAR_hasIntercom', 0] call TFAR_fnc_getVehicleConfigProperty) > 0 && !isNil {_target getVariable 'TFAR_IntercomPhoneSpeaker'}"; \
                 exceptions[] = {}; \
                 statement = " "; \
                 icon = QPATHTOF(ui\tfar_ace_interaction_intercom_phone_busy.paa); \
--- addons/intercom_phone/functions/fnc_activate.sqf
+++ addons/intercom_phone/functions/fnc_activate.sqf
@@ -22,14 +22,14 @@
 
 params ["_vehicle", "_player"];
 
-private _phoneSpeaker = _vehicle getVariable ["TFAR_IntercomPhoneSpeaker", nil];
+private _phoneSpeaker = _vehicle getVariable "TFAR_IntercomPhoneSpeaker";
 
 if (!isNil "_phoneSpeaker") exitWith {
     // [parseText format["<br /><br /><t align='center'>%1 is currently on the phone.</t>", name _phoneSpeaker]] call TFAR_fnc_showHint;
     "no";
 };
 
-private _vehicleID = _vehicle getVariable ["TFAR_vehicleIDOverride", netid _vehicle];
+private _vehicleID = _vehicle getVariable ["TFAR_vehicleIDOverride", netId _vehicle];
 _vehicle setVariable ["TFAR_vehicleIDOverride", _vehicleID, true];
 _vehicle setVariable ["TFAR_IntercomPhoneSpeaker", _player, true];
 _player setVariable ["TFAR_IntercomPhoneVehicle", _vehicle, true];
@@ -37,8 +37,9 @@ _player setVariable ["TFAR_vehicleIDOverride", _vehicleID, true];
 
 // Get intercom channel, now that we've signed on to the intercom
 private _intercomSlot = [_vehicle] call TFAR_intercom_phone_fnc_getIntercomChannel;
-_vehicle setVariable [format ["TFAR_IntercomSlot_%1",(netID _player)], _intercomSlot, true];
-_player setVariable [format ["TFAR_IntercomSlot_%1",(netID _player)], _intercomSlot, true]; // Because TFAR checks the "vehicle", and thinks the unit is the vehicle when it's not in a vehicle, so we manually set it
+
+_vehicle setVariable [format ["TFAR_IntercomSlot_%1",(netId _player)], _intercomSlot, true];
+_player setVariable [format ["TFAR_IntercomSlot_%1",(netId _player)], _intercomSlot, true]; // Because TFAR checks the "vehicle", and thinks the unit is the vehicle when it's not in a vehicle, so we manually set it
 
 // Show indicator
 if (TFAR_oldVolumeHint) then {
@@ -60,7 +61,7 @@ private _loopID = [_vehicle, _player] spawn {
 
 _player setVariable ["TFAR_IntercomPhoneLoopID", _loopID];
 
-// Just in case the caller gets into the vehicle
+// Just in case the caller gets into the vehicle or dies/disconnects
 _player setVariable ["TFAR_IntercomPhoneEHIDs", [
     _player addEventHandler ["GetInMan", {
         params ["_player", "_role", "_vehicle"];
