ece8cd0d8f3f0e5619b1f426b42f0b07e20d443c
Saborknight <support@saborknight.com>
2021-10-06 11:17:31 +1100

    Fixed - Ext. Intercom: Rope Segment not correctly referenced
    What
    =================
    The custom rope wasn't correctly referencing the custom rope segment, so it wasn't showing the custom wire model.
    
    Issue ID: N/A

--- addons/external_intercom/CustomRope.hpp
+++ addons/external_intercom/CustomRope.hpp
@@ -4,7 +4,7 @@ class CfgVehicles {
         maxRelLenght = 1.1;
         maxExtraLenght = 0;
         springFactor = 1; // higher == less stretchy rope
-        segmentType = "RopeSegment";
+        segmentType = "RopeSegmentSmallWire";
         torqueFactor = 0.5;
         dampingFactor[] = {1.0,2.5,1.0};
         model = QPATHTOF(data\TFAR_wire.p3d);
