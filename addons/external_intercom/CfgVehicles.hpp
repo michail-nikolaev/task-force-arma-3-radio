14e6f34261503f36465543c1de65dd18e9f49414
Saborknight <support@saborknight.com>
2021-10-08 13:52:56 +1100

    Changed - Ext Intercom: GetOut eventhandler to CBA XEH
    What
    =================
    Changed: The GetOut eventhandler to be a CBA Extended Eventhandler, and assigned to the player so that we don't have to worry about assigning it to particular vehicles.
    
    Issue ID: N/A

--- addons/external_intercom/CfgVehicles.hpp
+++ addons/external_intercom/CfgVehicles.hpp
@@ -1,24 +1,11 @@
 #include "ACEActions.hpp"
 
-#define ADD_EVENTHANDLERS \
-class EventHandlers : EventHandlers { \
-    class TFAR_ExternalIntercomWireless { \
-        getOut = QUOTE(params[ARR_3('_vehicle', '_role', '_player')]; if ( \
-            alive (_player) \
-            && isPlayer _player \
-            && (headgear (_player)) in TFAR_externalIntercomWirelessHeadgear \
-            && !((_role) isEqualTo 'cargo') \
-        ) then { [ARR_3(_vehicle, _player, [true])] call TFAR_external_intercom_fnc_connect; };); \
-    }; \
-};
-
 #define LAND_CLASS(parent, child) \
 class child : parent { \
     class ACE_Actions { \
         ADD_PHONE_ACTIONS \
         ADD_WIRELESS_ACTIONS \
     }; \
-    ADD_EVENTHANDLERS \
 };
 
 #define AIR_CLASS(parent, child) \
@@ -26,7 +13,6 @@ class child : parent { \
     class ACE_Actions { \
         ADD_WIRELESS_ACTIONS \
     }; \
-    ADD_EVENTHANDLERS \
 };
 
 class CfgVehicles {
@@ -52,4 +38,19 @@ class CfgVehicles {
 
     // Custom Interaction Points
     #include "CustomInteractionPoints.hpp"
+};
+
+class Extended_GetOutMan_EventHandlers {
+    class CAManBase {
+        class TFAR_ExternalIntercom {
+            clientGetOutMan = QUOTE(params[ARR_3('_player', '_role', '_vehicle')]; if ( \
+                alive (_player) \
+                && isPlayer _player \
+                && (headgear (_player)) in TFAR_externalIntercomWirelessHeadgear \
+                && !((_role) isEqualTo 'cargo') \
+                && local _player \
+                && [ARR_3((typeOf _vehicle), 'TFAR_hasIntercom', 0)] call TFAR_fnc_getVehicleConfigProperty == 1 \
+            ) then { diag_log format[ARR_3('%1 is getting out of %2', _player, _vehicle)]; [ARR_3(_vehicle, _player, [true])] call TFAR_external_intercom_fnc_connect; };);
+        };
+    };
 };
