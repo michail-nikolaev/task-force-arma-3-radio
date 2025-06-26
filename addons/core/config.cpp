da27cd5eb8e9064a3cf39689638cd02a0b05ccab
Smith <smitt14ua@gmail.com>
2025-06-26 19:20:13 +0300

    Localize encryption code in ACE Arsenal (#1578)

--- addons/core/config.cpp
+++ addons/core/config.cpp
@@ -135,7 +135,7 @@ class ace_arsenal_stats {
         scope = 2;
         priority = 1;
         stats[] = {"tf_encryptionCode"};
-        displayName = "Encryption Code";//#Todo Translate
+        displayName = CSTRING(radio_encryption_code);
         showText= 1;
         textStatement = QUOTE(params [ARR_2('_stat', '_config')]; private _enc = getText (_config >> _stat select 0); _enc);
         condition = QUOTE(call TFAR_fnc_statCondition_encryptionCode);
